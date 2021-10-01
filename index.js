d124fed8ce9bb830d95cdcbbd47cd11741f14ff6
soupaul <soupaul@expediagroup.com>
2021-10-01 16:17:59 +0530

    basic file structure made

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const port = 8000;
+
+const app = express();
+
+app.use(express.static('./assets'));
+app.use('/', require('./routes/index'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.listen(port, (err) => {
+    if(err) {
+        console.log("Error in running the server: ", err);
+        return;
+    }
+    console.log("Server is running perfectly fine on port: ", port);
+});
